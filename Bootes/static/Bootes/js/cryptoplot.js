12205a9fbf3836b710b854f9aec185ba4985502d
Ethan <e95tucker@gmail.com>
2018-05-22 18:27:18 -0400

    Tabbed graphs, major currency headbar

--- Bootes/static/Bootes/js/cryptoplot.js
+++ Bootes/static/Bootes/js/cryptoplot.js
@@ -1,24 +1,50 @@
-var margin = {top: 30, right: 20, bottom: 30, left: 100},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+function DataPlotter(lineData, contID, stripped){
 
-function DataPlotter(lineData){
-
+    if(stripped) {
+        var margin = {top: 5, right: 3, bottom: 5, left: 17},
+        width = 90 - margin.left - margin.right,
+        height = 50 - margin.top - margin.bottom;
+    } else {
+        var margin = {top: 30, right: 20, bottom: 30, left: 100},
+        width = 600 - margin.left - margin.right,
+        height = 300 - margin.top - margin.bottom;
+    }
+    // Tab/time specific svg containers
+    if(contID ==='today') {
+        svgContentID = 'TodayContent';
+    } else if(contID === 'btc') {
+        svgContentID = 'BTCContent';
+    } else if(contID === 'eth') {
+        svgContentID = 'ETHContent'
+    } else if(contID === 'xrp') {
+        svgContentID = 'XRPContent';
+    } else if(contID ==='month1') {
+        svgContentID = 'Month1Content';
+    } else if(contID ==='month6') {
+        svgContentID = 'Month6Content';
+    } else if(contID ==='year1') {
+        svgContentID = 'Year1Content';
+    } else if(contID ==='all') {
+        svgContentID = 'AllContent';
+    }  else {
+        console.log('Failed to locate data contID type')
+        return;
+    }
     var yScale = d3.scaleLinear()
         .domain(d3.extent(lineData.map(function(d) { return d.y; })))
-        .range([height,0]);
+            .range([height,0]);
 
     var xScale = d3.scaleTime()
         .domain(d3.extent(lineData.map(function(d) { return d.x; })))
-        .range([0,width]);
+            .range([0,width]);
 
     var xAxis = d3.axisBottom(xScale)
-        .ticks(10);
+        .ticks(5);
 
     var yAxis = d3.axisLeft(yScale)
-        .ticks(10);
+        .ticks(5);
 
-    var svgContainer = d3.select('#DataContent')
+    var svgContainer = d3.select(svgContentID)
         .append('svg')
         .attr('width', width + margin.left + margin.right)
         .attr('height', height + margin.top + margin.bottom)
@@ -32,13 +58,13 @@ function DataPlotter(lineData){
         .attr('class', 'axis')
         .attr('transform', 'translate(' + (width/8) + ',' + (height+margin.top) + ')')
         .call(xAxis);
-
+    
     svgContainer.append('text')
-        .attr('y', 160)
-        .attr('x', 30)
-        .attr('transform', 'rotate(90)')
-        .style('text-anchor', 'middle')
-        .text('Price ($)')
+            .attr('y', 160)
+            .attr('x', 30)
+            .attr('transform', 'rotate(90)')
+            .style('text-anchor', 'middle')
+            .text('Price ($)')
 
     svgContainer.append('g')
         .attr('class', 'axis')
@@ -63,7 +89,7 @@ function DataPlotter(lineData){
     focus.append("circle")
         .attr('transform', 'translate( ' + (width/8) + ',' + margin.top + ')')
         .attr("r", 4.5);
-
+    
     focus.append("text")
         .attr('transform', 'translate( ' + (width/8) + ',' + margin.top + ')')
         .attr("x", 9)
--- Bootes/views.py
+++ Bootes/views.py
@@ -16,14 +16,21 @@ import numpy as np
 # Create your views here.
 def DataSearch(request):
     template = loader.get_template('Bootes/BootesHome.html')
-    def days_historical_data(symbol, comparison_symbol='USD', all_data=True, limit=1, aggregate=1, exchange=''):
-        url='https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit={}&aggregate={}'\
-        .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate, exchange)
+
+    #Consume cryptocompare API for desired cryptocurrency data
+    def historical_data(symbol, timediv, comparison_symbol='USD', all_data=True, limit=1, aggregate=1, exchange='', ):
+        if timediv == 'minute':
+            url='https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit={}&aggregate={}'\
+            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate, exchange)
+        if timediv == 'day':
+            url='https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}&limit={}&aggregate={}'\
+            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate, exchange)
         if not exchange:
             exchange = 'CCCAGG'
             url+= '&e={}'.format(exchange)
         if all_data:
             url+='&allData=True'
+        print(url)
         page=requests.get(url)
         data=page.json()['Data']
         df=pd.DataFrame(data)
@@ -34,15 +41,55 @@ def DataSearch(request):
             print('empty dataframe')
             df=""
         return df
+
+    #Obtain today's major cryptocurrency data and format appropriately for d3 render
+    Q = 'BTC', 'ETH', 'XRP'
+    Q1_hist = historical_data(Q[0], 'minute').tail(1440)
+    Q2_hist = historical_data(Q[1], 'minute').tail(1440)
+    Q3_hist = historical_data(Q[2], 'minute').tail(1440)
+
+    #adjust timedata offset for tz:America/Toronto
+    Q1_hist['timestamp'] += datetime.timedelta(hours=5)
+    Q2_hist['timestamp'] += datetime.timedelta(hours=5)
+    Q3_hist['timestamp'] += datetime.timedelta(hours=5)
+
+    #Get current price for major cryptocurrencies
+    Q1_now = cryptocompare.get_price(Q[0], 'USD')[Q[0]]['USD']
+    Q2_now = cryptocompare.get_price(Q[1], 'USD')[Q[1]]['USD']
+    Q3_now = cryptocompare.get_price(Q[2], 'USD')[Q[2]]['USD']
+    print(type(Q1_now))
+
+    #Format data for d3.js
+    Q1_hist = pd.DataFrame.to_json(Q1_hist[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
+    Q2_hist = pd.DataFrame.to_json(Q2_hist[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
+    Q3_hist = pd.DataFrame.to_json(Q3_hist[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
+
+    #Obtain cryptocurrency data and format appropriately for d3 rendering
     if request.method == 'GET' and request.GET.get('search_box',None) != None:
         search_query = request.GET.get('search_box', None)
         TickSym=search_query.upper()
-        currency_data = cryptocompare.get_price(search_query.upper(), 'USD')[TickSym]['USD']
-        current_time = datetime.datetime.now()
-        histo_crypto = days_historical_data(search_query.upper()).tail(1440)
-        histo_crypto['timestamp'] += datetime.timedelta(hours=5) #adjust cryptocompare timedata offset for tz:America/Toronto
-        histo_crypto = pd.DataFrame.to_json(histo_crypto[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
-        return HttpResponse(template.render({"currency_data" : currency_data, "current_time" : current_time, "histo_crypto" : histo_crypto, 'TickSym' : TickSym}))
+        CurrentPrice = cryptocompare.get_price(search_query.upper(), 'USD')[TickSym]['USD']
+        CurrentTime = datetime.datetime.now()
+        
+        #Store data in time increments for tab display
+        m1_hist = historical_data(search_query.upper(), 'minute').tail(43200) #1 month
+        daily_hist = m1_hist.tail(1440) #1 day
+        all_hist = historical_data(search_query.upper(), 'day') #all time
+        m6_hist = all_hist.tail(183)#6 months
+        y1_hist = all_hist.tail(365)#1 year
+
+        #adjust cryptocompare timedata offset for tz:America/Toronto
+        m1_hist['timestamp'] += datetime.timedelta(hours=5)
+        daily_hist['timestamp'] += datetime.timedelta(hours=5)
+
+        #Format data for d3.js
+        m1_hist = pd.DataFrame.to_json(m1_hist[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
+        daily_hist = pd.DataFrame.to_json(daily_hist[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
+        m6_hist = pd.DataFrame.to_json(m6_hist[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
+        y1_hist = pd.DataFrame.to_json(y1_hist[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
+        all_hist = pd.DataFrame.to_json(all_hist[['timestamp', 'close']].rename(index=str, columns={'timestamp':'x', 'close':'y'}), orient='records')
+        
+        return HttpResponse(template.render({"Q1" : Q[0], "Q2" : Q[1], "Q3" : Q[2], "Q1_now" : Q1_now, "Q2_now" : Q2_now, "Q3_now" : Q3_now, "Q1_hist" : Q1_hist, "Q2_hist" : Q2_hist, "Q3_hist" : Q3_hist, "CurrentPrice" : CurrentPrice, "CurrentTime" : CurrentTime, "daily_hist" : daily_hist, "m1_hist" : m1_hist, "m6_hist" : m6_hist, "y1_hist" : y1_hist, "all_hist" : all_hist, "TickSym" : TickSym}))
     else:
         search_query = "You haven't entered anything."
         return HttpResponse(template.render())
